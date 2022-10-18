e968ea8e53bd4fb6164bfe4b1ece786d6aab7d62
marcos-nnd <marcos.paixao@sptech.school>
2022-10-18 13:20:54 -0300

    site vINFINITY

--- Marcos-Fernando_01222140/agrisoft/public/javascript/calculadora.js
+++ Marcos-Fernando_01222140/agrisoft/public/javascript/calculadora.js
@@ -21,17 +21,46 @@
         <div class="div-calc">
         <h4>Resultado</h4><br>
         <u>
-            <li>Seu faturamento: ${receita} por safra</li><br>
-            <li>Sua Perda: ${prejuizo} por safra</li><br>
-            <li>Sua Receita final: ${receita_perda.toFixed(2)}</li><br>
-            <li>Você perdeu aproximadamente ${porcentagem_perda.toFixed(2)} %.</li>
+            <li>Seu faturamento: ${receita.toLocaleString(
+                "pt-BR", {
+                    style: "currency", 
+                    currency: "BRL", 
+                    maximumFractionDigits: 2}
+                )} por safra</li><br>
+
+            <li>Sua Perda: ${prejuizo.toLocaleString(
+                "pt-BR", {
+                    style: "currency", 
+                    currency: "BRL", 
+                    maximumFractionDigits: 2}
+                )} por safra</li><br>
+
+            <li>Sua Receita final: ${receita_perda.toLocaleString(
+                "pt-BR", {
+                    style: "currency", 
+                    currency: "BRL", 
+                    maximumFractionDigits: 2}
+                )}</li><br>
+
+            <li>Você perdeu aproximadamente ${porcentagem_perda.toLocaleString(
+                "pt-BR", {
+                    style: "currency", 
+                    currency: "BRL", 
+                    maximumFractionDigits: 2}
+                )} %.
+            </li>
         </u>
         </div>
 
         <div class="div-calc">
         <u>
         <h4>Com SoyTech</h4><br>
-            <li>A partir de resultados registrados com a utilização do produto SoyTech, sua receita com prejuizo reduzido em 30% resultou em ${valor_final.toFixed(2)} </li><br>
+            <li>A partir de resultados registrados com a utilização do produto SoyTech, sua receita com prejuizo reduzido em 30% resultou em 
+            ${valor_final.toLocaleString( "pt-BR", {
+                    style: "currency", 
+                    currency: "BRL", 
+                    maximumFractionDigits: 2}
+                )} </li><br>
         </u>
         </div>
 
